b17574ac9a4898c11667f9630825c0860eb3f790
g7d52691379004 <g7d52691379004@p3nlwpsftp001.shr.prod.phx3.secureserver.net>
2015-10-01 21:58:46 -0700

    Jack update

---
+++ wp-content/themes/geodir_geo-travel/template-cruises-blog.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Template name: Cruise Blog
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage classiads
+ * @since classiads 1.2.2
+ */
+
+?>
+
+<?php include_once "template-travel-header.php";?>
+	<div class="site-inner">
+		<div class="wrap">
+			<div class="content-sidebar-wrap">
+				<main class="content" role="main">
+					<?php 
+						$posts = query_posts( array('cat' => 3, 'posts_per_page' => get_option('posts_per_page')) );
+						while ( have_posts() ) : the_post();
+					?>
+						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<header class="entry-header">
+								<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+							</header>
+							<div class="entry-content">
+								<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+									<?php the_post_thumbnail('medium'); ?>
+								</a>
+								<p>
+								<?php $content = get_the_content(); $content1 = strip_shortcodes( $content ); $content11 = strip_tags($content1); echo substr($content11, 0, 395).' <a href="'.get_permalink().'"> Read More </a>';?>
+								</p>
+							</div>
+							<footer class="entry-footer">
+								<p class="entry-meta">
+									<span class="entry-categories">Filed Under: 
+										<?php 
+											$cate = get_the_category(); 
+											foreach($cate as $cates)
+											{
+												$link = get_term_link( $cates->term_id, 'category' );
+												echo '<a href="'.$link.'" rel="category tag">'.$cates->cat_name.'</a>';
+											}
+										?>
+									</span> 
+									<!--<span class="entry-tags">Tagged With: </span>-->
+								</p>
+							</footer>
+						</article>
+					<?php 
+						endwhile;
+					wp_reset_query();
+					?>
+                </main>
+			</div>
+		</div>
+	</div>
+<?php include_once "template-travel-footer.php";?>

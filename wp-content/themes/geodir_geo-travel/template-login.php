6239c4fe6af73e31da848411b683b11da84a272b
Jack Carrick Xiang Cheng <chengxianga2008@yahoo.com>
2015-10-20 15:13:54 +1100

    Jack update

---
+++ wp-content/themes/geodir_geo-travel/template-login.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template name: Login Page
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage classiads
+ * @since classiads 1.2.2
+ */
+
+?>
+
+<?php include_once "template-travel-header.php";?>
+
+<?php /*
+echo do_shortcode('[layerslider id="2"]');
+*/
+?> 
+
+
+
+
+<?php include_once "template-travel-footer.php";?>

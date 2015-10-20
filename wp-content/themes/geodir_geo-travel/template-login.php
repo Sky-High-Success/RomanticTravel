7f44a83e47fea9384dbce43a39bbf8ef747681d8
Jack Carrick Xiang Cheng <chengxianga2008@yahoo.com>
2015-10-20 15:33:56 +1100

    Jack update

--- wp-content/themes/geodir_geo-travel/template-login.php
+++ wp-content/themes/geodir_geo-travel/template-login.php
@@ -11,7 +11,23 @@
 
 ?>
 
-<?php include_once "template-travel-header.php";?>
+<?php 
+
+wp_enqueue_style( 'material-style', get_stylesheet_directory_uri() . '/css/material.min.css', array(), '1' );
+
+wp_enqueue_style( 'ripples-style', get_stylesheet_directory_uri() . '/css/ripples.min.css', array(), '1' );
+
+wp_enqueue_style( 'Boxes-style', get_stylesheet_directory_uri() . '/css/Boxes.css', array(), '1' );
+
+wp_enqueue_script( 'material', get_stylesheet_directory_uri() . '/js/material.min.js', array( 'jquery' ), '2014-07-18', true );
+
+wp_enqueue_script( 'ripples', get_stylesheet_directory_uri() . '/js/ripples.min.js', array( 'jquery' ), '2014-07-18', true );
+
+
+
+include_once "template-travel-header.php";?>
+
+
 
 <?php /*
 echo do_shortcode('[layerslider id="2"]');
